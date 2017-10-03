1972ddaaa5e0c1f36535b34f6260d72ca2bd9514
Dmytro_Holysh <Dmytro_Holysh@epam.com>
2017-10-03 12:27:49 +0300

    tasks 1-4 done

--- src/app/services/config-options.service.ts
+++ src/app/services/config-options.service.ts
@@ -3,6 +3,23 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConfigOptionsService {
 
-  constructor() { }
+  constructor(private config: Config) { }
 
+  getLogin() {
+    return this.config.login;
+  }
+
+  getEmail() {
+    return this.config.email;
+  }
+
+  getId() {
+    return this.config.id;
+  }
+}
+
+class Config {
+  login: string;
+  email: string;
+  id: number;
 }
--- src/app/services/constants.service.ts
+++ src/app/services/constants.service.ts
@@ -3,6 +3,13 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConstantsService {
 
+  private appConfig = {
+    App: 'TaskManager',
+    Ver: '1.0'
+  };
   constructor() { }
 
+  getConstants() {
+    return this.appConfig;
+  }
 }
